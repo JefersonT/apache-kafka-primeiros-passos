3c487df74c461cfc35eb6077ec6e9049682fc594
Jeferson Torres <jefersontorres.f@gmail.com>
2022-05-13 16:12:18 -0300

    Adiconando serviço http para nova ordem

---
+++ ecommerce/service-httpecommerce/src/main/java/br/com/alura/ecommerce/NewOrderServlet.java
@@ -0,0 +1,59 @@
+package br.com.alura.ecommerce;
+
+import jakarta.servlet.Servlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.eclipse.jetty.servlet.Source;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+
+public class NewOrderServlet extends HttpServlet {
+    private final KafkaDispatcher<Order> orderDispatcher = new KafkaDispatcher<>();/* Criando um KafkaDispatcher para cria um Producer*/
+    private final KafkaDispatcher<String> emailDispatcher = new KafkaDispatcher<>();/* Criando um KafkaDispatcher para cria um Producer*/
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        orderDispatcher.close();
+        emailDispatcher.close();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            /*Criando email aleatório temporarioamente*/
+            var email = req.getParameter("email");
+
+            /* Declarando um orderID Aleatório*/
+            var orderId = UUID.randomUUID().toString();
+
+            /* Declarando o amount, o valor da orden em BigDecimal*/
+            var amount = BigDecimal.valueOf(Long.parseLong(req.getParameter("amount"))); /* Valor entre 1 e 5000*/
+
+            /* Criando uma nova Order*/
+            var order = new Order(orderId, amount, email);
+            /* Enviando a orden com o userID para o topic ECOMMERCE_NEW_ORDER*/
+            orderDispatcher.send("ECOMMERCE_NEW_ORDER", email, order);
+
+            /* Denifindo o valor para o metodo send*/
+            var emailCode = "Thanks You for your new Order!";
+
+            /* Enviando a Email com o userID para o topic ECOMMERCE_SEND_EMAIL*/
+            emailDispatcher.send("ECOMMERCE_SEND_EMAIL", email, emailCode);
+
+            System.out.println("New order sent successfully!");
+            resp.setStatus(HttpServletResponse.SC_OK);
+            resp.getWriter().println("New order sent!");
+
+        } catch (ExecutionException e) {
+            throw new ServletException(e);
+        } catch (InterruptedException e) {
+            throw new ServletException(e);
+        }
+    }
+}
---
+++ ecommerce/service-httpecommerce/src/main/java/br/com/alura/ecommerce/Order.java
@@ -0,0 +1,19 @@
+package br.com.alura.ecommerce;
+
+import java.math.BigDecimal;
+
+/* Classe Order para utilizar como mensagem de envio referente*/
+public class Order {
+
+    /* Dados da ordem, ID do usuário, ID da ordem e valor da ordem*/
+    private final String orderId;
+    private final BigDecimal value;
+    private final String email;
+
+    /* Contrutor recebando os dados da Ordem*/
+    public Order(String orderId, BigDecimal value, String email) {
+        this.orderId = orderId;
+        this.value = value;
+        this.email = email;
+    }
+}
